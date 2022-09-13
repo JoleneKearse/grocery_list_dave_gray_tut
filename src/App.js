add6e2f4c076c6788468ab6074cdddf9745b6a2b
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-12 21:16:18 -0400

    up to controlled input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Old Cheddar Cheese Sticks",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Lemon",
+    },
+  ]);
+
+  const [newItem, setNewItem] = useState("");
+
+  const handleCheck = (id) => {
+    // create new listItems array to alter checked status
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shopinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleSubmit = (e) => {
+    console.log("submitted");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
---
+++ src/LineItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        // need anonymous function to pass in id
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+        title="Delete item"
+        aria-label={`Delete ${item.item}`}
+      />
+    </li>
+  );
+};
+
+export default LineItem;
