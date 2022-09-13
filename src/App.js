f8e131ba19f3de67f9bda6767d28571c2c311ad3
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-13 18:18:25 -0400

    Added search, fixed css order

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Old Cheddar Cheese Sticks",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Lemon",
-    },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shopinglist"))
+  );
 
   const [newItem, setNewItem] = useState("");
+
+  const [search, setSearch] = useState("");
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
@@ -67,8 +56,11 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <SearchItem 
+        search={search} 
+        setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
