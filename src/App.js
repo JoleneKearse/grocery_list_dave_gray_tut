62fb6deeb47b2bcd9c5f0d2dd3ad8c254f4f5fd0
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-26 17:13:55 -0400

    finished useEffect Hooks code

--- src/App.js
+++ src/App.js
@@ -3,28 +3,27 @@ import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shopinglist"))
+    JSON.parse(localStorage.getItem("shopinglist")) || []
   );
 
   const [newItem, setNewItem] = useState("");
 
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shopinglist", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("shopinglist", JSON.stringify(items));
+  }, [items]);
 
   const addItem = (item) => {
     // add next id value. Check if there are items. Subtract 1 from items array length. Access id and add 1. Or return 1
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
@@ -32,17 +31,16 @@ function App() {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // because AddItem.js form input has required attribute, it shouldn't be blank, but we can check anyway
     if (!newItem) return;
     addItem(newItem);
     setNewItem("");
@@ -56,11 +54,11 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem 
-        search={search} 
-        setSearch={setSearch} />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
